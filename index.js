2dac102a8703df5bcc6fc6a9520dc0275a283459
samikshyaadhikari098 <samikshya.adhikari098@gmail.com>
2024-05-21 10:33:43 -0500

    Commited the changes

---
+++ index.js
@@ -0,0 +1,32 @@
+let change = document.getElementById("change")
+let change2 = document.getElementById("change2")
+
+let co = 0
+let c=0
+
+function plus(){
+    c +=1;
+    change.innerHTML = c
+
+}
+function plus2(){
+    c +=2;
+    change.innerHTML = c
+
+}
+function plus3(){
+    c +=3;
+    change.innerHTML = c
+}
+function plus4(){
+    co +=1
+    change2.innerHTML = co
+}
+function plus5(){
+    co+=2
+    change2.innerHTML = co
+}
+function plus6(){
+    co+=3
+    change2.innerHTML = co
+}
